21602fbfa125e85def03210fbe3b016bb4a54291
Muhamadzidann <muhamadzidan@smkwikrama.sch.id>
2024-01-05 07:50:37 +0700

    first commit

---
+++ faktorial.js
@@ -0,0 +1,10 @@
+ //Faktorial For
+ let angka = 5;
+ let faktorial = 0;al
+
+ for(let i = 1; i <= angka; i++){
+     faktorial += i;
+ }
+ document.write(`Faktorial dari ${angka} adalah ${faktorial}`)
+
+ //Faktorial While
